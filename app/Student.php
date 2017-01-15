6be5a906422ce139eb39aceeae64a0b87b8943f4
Shobi <ppshobi@gmail.com>
2017-01-15 18:27:42 +0530

    New Student Class

---
+++ app/Student.php
@@ -0,0 +1,80 @@
+<?php 
+	/**
+	* 
+	*/
+	require_once('DB.php');
+	class Student
+	{
+		public static function add($name,$regno,$dept,$batch){
+			$db = new Db();
+			$name=$db -> quote($name);
+			$regno=$db -> quote($regno);
+			$dept=$db -> quote($dept);
+			$batch=$db -> quote($batch);
+
+			$sql="INSERT INTO student(name,regno,dept,batch) VALUES('$name', '$regno', '$dept', '$batch')";
+			$result=$db -> query($sql);
+			if($result){
+				return true;
+			}else{	
+				return false;
+			}
+		}
+
+		public static function edit($id,$name,$regno,$dept,$batch){
+			$db = new Db();
+			$name=$db -> quote($name);
+			$regno=$db -> quote($regno);
+			$dept=$db -> quote($dept);
+			$batch=$db -> quote($batch);
+
+			$sql=" UPDATE student SET name = '$name', regno = '$regno',dept = '$dept',batch = '$batch' WHERE id= '$id' ";
+			$result=$db -> query($sql);
+			if($result){
+				return true;
+			}else{		
+
+				return false;
+			}
+		}
+
+		public static function getAll(){
+			$db=new Db();
+			$sql="SELECT * FROM student";
+			$rows=[];
+			$result=$db->query($sql);
+			if($result){
+				while ($r=mysqli_fetch_assoc($result)) {
+					array_push($rows, $r);
+				}
+				return $rows;
+			}
+			return false;
+			
+		}
+		public static function getOne($id){
+			$db=new Db();
+			$sql="SELECT * FROM student WHERE id= $id LIMIT 1";
+			$rows=[];
+			$result=$db->query($sql);
+			if($result){
+				while ($r=mysqli_fetch_assoc($result)) {
+					array_push($rows, $r);
+				}
+				return $rows[0];
+			}
+			return false;
+			
+		}
+		public static function delete($id){
+			$db=new Db();
+			$sql="DELETE FROM student WHERE id='$id' LIMIT 1";
+			$result=$db->query($sql);
+			if($result){				
+				return true;
+			}
+			return false;
+		}
+
+	}
+?>
