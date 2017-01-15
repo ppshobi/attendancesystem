7967c5c4bc3db158674dbf644f9647d70e75e127
Shobi <ppshobi@gmail.com>
2017-01-15 19:34:15 +0530

    Edit Student Done

--- edit-student.php
+++ edit-student.php
@@ -1,20 +1,22 @@
-n<?php
+<?php
 //login check
 
-require_once('app/Department.php');
 require_once('app/Student.php');
+require_once('app/Department.php');
 $message;
-if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['deletestudent'])) {
+$departments=Department::getAll();
+$student;
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['editstudent'])) {
 	
-	$result=Student::delete($_POST['studentid']);
+	$result=Student::edit($_POST['studentid'],$_POST['name'], $_POST['regno'], $_POST['dept'],$_POST['batch']);
 	if ($result) {
 		$message=true;
 	}else{
 		$message=false;
 	}
+}else{
+	$student=Student::getOne($_GET['id']);
 }
-$depts=Department::getAll();
-$students=Student::getAll();
 ?>
 <!DOCTYPE html>
 <html>
@@ -22,7 +24,7 @@ $students=Student::getAll();
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
 	<meta http-equiv="x-ua-compatible" content="ie=edge">
-	<title>Manage Student</title>
+	<title>Edit a Student</title>
 
 	<link href="img/favicon.144x144.png" rel="apple-touch-icon" type="image/png" sizes="144x144">
 	<link href="img/favicon.114x114.png" rel="apple-touch-icon" type="image/png" sizes="114x114">
@@ -57,7 +59,7 @@ $students=Student::getAll();
 				<div class="tbl">
 					<div class="tbl-row">
 						<div class="tbl-cell">
-							<h3>Manage Students</h3>
+							<h3>Add Student</h3>
 							<ol class="breadcrumb breadcrumb-simple">
 								<li><a href="#">StartUI</a></li>
 								<li><a href="#">Forms</a></li>
@@ -75,52 +77,50 @@ $students=Student::getAll();
 
 				<h5 class="m-t-lg with-border">Enter Details</h5>
 
-				<div class="card-block">
-					<table id="example" class="display table table-striped table-bordered" cellspacing="0" width="100%">
-						<thead>
-						<tr>
-							<th>Sl.No</th>
-							<th>Student Name</th>
-							<th>Register No</th>
-							<th>Department</th>
-							<th>Batch</th>
-							<th colspan="2">Options</th>
-						</tr>
-						</thead>
-						<tfoot>
-						<tr>
-							<th>Sl.No</th>
-							<th>Student Name</th>
-							<th>Register No</th>
-							<th>Department</th>
-							<th>Batch</th>
-							<th colspan="2">Options</th>
-						</tr>
-						</tfoot>
-						<tbody>
-						<?php
-						if($students){
-							$count=1;
-							foreach ($students as $student) {
-								$dept=Department::getOne($student['dept']);
-								echo "<tr>";
-									echo "<td>" . $count ."</td>";
-									echo "<td>" . $student['name'] ."</td>";
-									echo "<td>" . $student['regno'] ."</td>";
-									echo "<td>" . $dept['name'] ."</td>";
-									echo "<td>" . $student['batch'] ."</td>";
-									echo "<td>" . "<a href=\"edit-student.php?id=".$student['id']."\" class=\"btn btn-rounded btn-inline btn-warning\" >"."Edit</a></td>";
-									echo "<td>" . "<button value=".$student['id']." class=\"btn btn-rounded btn-inline btn-danger swal-btn-cancel\" >"."Delete</button></td>";
-									
-		 						echo "</tr>";
-		 						$count++;
-	 						}
-						}
-						?>
-						
-						</tbody>
-					</table>
-				</div>
+				<form method="post" id="student" action="">
+					<div class="form-group row">
+						<label for="TeacherName" class="col-sm-2 form-control-label">Student Name</label>
+						<div class="col-sm-10">
+							<p class="form-control-static"><input id="name" type="text" name="name" class="form-control" placeholder="Student Name" value="<?php echo $student['name']; ?>"></p>
+						</div>
+					</div>
+					<div class="form-group row">
+						<label for="Register no" class="col-sm-2 form-control-label">Register No</label>
+						<div class="col-sm-10">
+							<p class="form-control-static"><input id="regno" value="<?php echo $student['regno']; ?>" type="text" name="regno" class="form-control" placeholder="Register No"></p>
+						</div>
+					</div>
+					<div class="form-group row">
+						<label for="Department" class="col-sm-2 form-control-label">Select Department</label>
+						<div class="col-sm-10">
+							<select name="dept" id="dept" class="select2">
+							<option value="<?php echo $student['dept']; ?>" >Select Department</option>
+							 	<?php
+							 		foreach ($departments as $dept) {
+							 			echo "<option value=\"".$dept['id']."\">".$dept['name']."</option>";
+							 		}
+							 	?>	
+							</select>
+						</div>
+					</div>
+					<div class="form-group row">
+						<label for="Batch" class="col-sm-2 form-control-label">Select Batch</label>
+						<div class="col-sm-10">
+								<input type="radio" name="batch" value="1" <?php echo $student['batch']==1?"checked":""; ?>/>1 Year				
+								<input type="radio" name="batch" value="2"<?php echo $student['batch']==2?"checked":""; ?> />2 Year	
+ 								<input type="radio" name="batch" value="3" <?php echo $student['batch']==3?"checked":""; ?>/>3 Year
+						</div>
+					</div>
+					<input type="hidden" id="studentid" name="studentid" value="<?php echo $student['id']; ?>">
+					<div class="form-group row">
+						<label for="button" class="col-sm-2 form-control-label"></label>
+						<div class="col-sm-10">
+							<button type="submit" name="edit-stud" class="btn btn-inline btn-success-outline swal-btn-success">Update Student</button>
+						</div>
+					</div>
+					
+				</form>
+
 
 			</div><!--.box-typical-->
 		</div><!--.container-fluid-->
@@ -130,56 +130,41 @@ $students=Student::getAll();
 	<script src="js/lib/tether/tether.min.js"></script>
 	<script src="js/lib/bootstrap/bootstrap.min.js"></script>
 	<script src="js/plugins.js"></script>
-
 	<script src="js/lib/select2/select2.full.min.js"></script>
 	<script src="js/lib/bootstrap-touchspin/jquery.bootstrap-touchspin.min.js"></script>
 	<script src="js/lib/bootstrap-sweetalert/sweetalert.min.js"></script>
-<script type="text/javascript">
-	$('.swal-btn-cancel').click(function(e){
-		var studentid=$(this).val();
+	<script type="text/javascript">
+	$('.swal-btn-success').click(function(e){
 		e.preventDefault();
-			swal({
-					title: "Are you sure?",
-					text: "You will not be able to undo this Opration!",
-					type: "warning",
-					showCancelButton: true,
-					confirmButtonClass: "btn-danger",
-					confirmButtonText: "Yes, delete it!",
-					cancelButtonText: "No, cancel!",
-					closeOnConfirm: false,
-					closeOnCancel: false
-				},
-				function(isConfirm) {
-					if (isConfirm) {
-						$.ajax({
-				                type: "POST",
-				                url: "manage-student.php",
-				                data: { 
-				                	studentid : studentid,
-				                    deletestudent:true
-				                }
-				            }).success(function(msg){
-				               swal({
-									title: "Deleted!",
-									text: "The Student is Deleted.",
-									type: "success",
-									confirmButtonClass: "btn-success"
-								});
-				               location.reload();
-				        });
-						
-					} else {
-						swal({
-							title: "Cancelled",
-							text: "The Student is not Deleted :)",
-							type: "error",
-							confirmButtonClass: "btn-danger"
-						});
-					}
+		var name=$("#name").val();
+		var studentid=$("#studentid").val();
+		var regno=$("#regno").val();
+		var dept=$("#dept").val();
+		var batch=$('input[name=batch]:checked', '#student').val();
+		
+		$.ajax({
+                type: "POST",
+                url: "edit-student.php",
+                data: { 
+                    studentid: studentid,
+                    name: name,
+                    regno: regno,
+                    dept: dept,
+                    batch: batch,
+                    editstudent: true
+                }
+            }).success(function(msg){
+                swal({
+					title: "Good job!",
+					text: "Updated the Student!",
+					type: "success",
+					confirmButtonClass: "btn-success",
+					confirmButtonText: "OK"
 				});
-	});
+        	});
+		
+		});
 </script>
-
 <script src="js/app.js"></script>
 </body>
 </html>
