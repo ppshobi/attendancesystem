cb252a9c88daf8b4089c42c056c6b8b83352c3b0
Shobi <ppshobi@gmail.com>
2017-01-24 19:16:25 +0530

    Nav update

--- nav.php
+++ nav.php
@@ -1,354 +1,81 @@
 <nav class="side-menu">
-	    <ul class="side-menu-list">
-	        <li class="grey with-sub">
-	            <span>
-	                <i class="font-icon font-icon-dashboard"></i>
-	                <span class="lbl">Dashboard HOD</span>
-	            </span>
-	           
-	        </li>
-	        <li class="grey with-sub">
-	            <span>
-	                <i class="font-icon font-icon-dashboard"></i>
-	                <span class="lbl">Departments</span>
-	            </span>
-	            <ul>
-	                <li><a href="add-department.php">Add a Department</a></li>
-	                <li><a href="manage-department.php">Manage</a></li>
-	                
-	            </ul>
-	        </li>
-	        <li class="grey with-sub">
-	            <span>
-	                <i class="font-icon font-icon-dashboard"></i>
-	                <span class="lbl">Students</span>
-	            </span>
-	            <ul>
-	                <li><a href="add-student.php">Add a Student</a></li>
-	                <li><a href="manage-student.php">Manage</a></li>
-	                
-	            </ul>
+    <ul class="side-menu-list">
+        <li class="grey with-sub">
+            <span>
+                <i class="font-icon font-icon-dashboard"></i>
+                <span class="lbl">Dashboard HOD</span>
+            </span>
+           
+        </li>
+        <li class="grey with-sub">
+            <span>
+                <i class="font-icon font-icon-dashboard"></i>
+                <span class="lbl">Departments</span>
+            </span>
+            <ul>
+                <li><a href="add-department.php">Add a Department</a></li>
+                <li><a href="manage-department.php">Manage</a></li>
+                
+            </ul>
+        </li>
+        <li class="grey with-sub">
+            <span>
+                <i class="font-icon font-icon-dashboard"></i>
+                <span class="lbl">Students</span>
+            </span>
+            <ul>
+                <li><a href="add-student.php">Add a Student</a></li>
+                <li><a href="manage-student.php">Manage</a></li>
+                
+            </ul>
 
-	       </li>
-	        <li class="grey with-sub">
-	            <span>
-	                <i class="font-icon font-icon-dashboard"></i>
-	                <span class="lbl">Teachers</span>
-	            </span>
-	            <ul>
-	                <li><a href="add-teacher.php">Add a Teacher</a></li>
-	                <li><a href="manage-teacher.php">Manage</a></li>
-	                
-	            </ul>
-	        </li>
-	        <li class="grey with-sub">
-	            <span>
-	                <i class="font-icon font-icon-dashboard"></i>
-	                <span class="lbl">HOD</span>
-	            </span>
-	            <ul>
-	                <li><a href="add-hod.php">Add a HOD</a></li>
-	                <li><a href="manage-hod.php">Manage</a></li>
-	                
-	            </ul>
-	        </li>
-	        <li class="grey with-sub">
-	            <span>
-	                <i class="font-icon font-icon-dashboard"></i>
-	                <span class="lbl">Binding</span>
-	            </span>
-	            <ul>
-	                <li><a href="index.php">Binding HOD to Department</a></li>
-	                <li><a href="dashboard-top-menu.php">Binding Teachers to Periods</a></li>
-	                
-	            </ul>
-	        </li>
-	        <li class="grey with-sub">
-	            <span>
-	                <i class="font-icon font-icon-dashboard"></i>
-	                <span class="lbl">Reset</span>
-	            </span>
-	            <ul>
-	                <li><a href="index.php">Year Change</a></li>
-	                <li><a href="semester-change.php">Semester change</a></li>
-	                <li><a href="dashboard-top-menu.php">Backup</a></li>
-	                
-	            </ul>
-	        </li>
-	        <li class="brown with-sub">
-	            <span>
-	                <i class="font-icon glyphicon glyphicon-tint"></i>
-	                <span class="lbl">Skins</span>
-	            </span>
-	            <ul>
-	                <li><a href="theme-side-ebony-clay.php"><span class="lbl">Ebony Clay</span></a></li>
-	                <li><a href="theme-side-madison-caribbean.php"><span class="lbl">Madison Caribbean</span></a></li>
-	                <li><a href="theme-side-caesium-dark-caribbean.php"><span class="lbl">Caesium Dark Caribbean</span></a></li>
-	                <li><a href="theme-side-tin.php"><span class="lbl">Tin</span></a></li>
-	                <li><a href="theme-side-litmus-blue.php"><span class="lbl">Litmus Blue</span></a></li>
-	                <li><a href="theme-rebecca-purple.php"><span class="lbl">Rebecca Purple</span></a></li>
-	                <li><a href="theme-picton-blue.php"><span class="lbl">Picton Blue</span></a></li>
-	                <li><a href="theme-picton-blue-white-ebony.php"><span class="lbl">Picton Blue White Ebony</span></a></li>
-	            </ul>
-	        </li>
-	        <li class="purple with-sub">
-	            <span>
-	                <i class="font-icon font-icon-comments active"></i>
-	                <span class="lbl">Messages</span>
-	            </span>
-	            <ul>
-	                <li><a href="messenger.php"><span class="lbl">Messenger</span></a></li>
-	                <li><a href="chat.php"><span class="lbl">Messages</span><span class="label label-custom label-pill label-danger">8</span></a></li>
-	                <li><a href="chat-write.php"><span class="lbl">Write Message</span></a></li>
-	                <li><a href="chat-index.php"><span class="lbl">Select User</span></a></li>
-	            </ul>
-	        </li>
-	        <li class="red">
-	            <a href="mail.php">
-	                <i class="font-icon glyphicon glyphicon-send"></i>
-	                <span class="lbl">Mail</span>
-	            </a>
-	        </li>
-	        <li class="gold with-sub">
-	            <span>
-	                <i class="font-icon font-icon-edit"></i>
-	                <span class="lbl">Forms</span>
-	            </span>
-	            <ul>
-	                <li><a href="ui-form.php"><span class="lbl">Basic Inputs</span></a></li>
-	                <li><a href="ui-buttons.php"><span class="lbl">Buttons</span></a></li>
-	                <li><a href="ui-select.php"><span class="lbl">Select &amp; Tags</span></a></li>
-	                <li><a href="ui-checkboxes.php"><span class="lbl">Checkboxes &amp; Radios</span></a></li>
-	                <li><a href="ui-form-validation.php"><span class="lbl">Validation</span></a></li>
-	                <li><a href="typeahead.php"><span class="lbl">Typeahead</span></a></li>
-	                <li><a href="steps.php"><span class="lbl">Steps</span></a></li>
-	                <li><a href="ui-form-input-mask.php"><span class="lbl">Input Mask</span></a></li>
-	                <li><a href="ui-form-extras.php"><span class="lbl">Extras</span></a></li>
-	            </ul>
-	        </li>
-	        <li class="blue-dirty">
-	            <a href="tables.php">
-	                <span class="glyphicon glyphicon-th"></span>
-	                <span class="lbl">Tables</span>
-	            </a>
-	        </li>
-	        <li class="magenta with-sub">
-	            <span>
-	                <span class="glyphicon glyphicon-list-alt"></span>
-	                <span class="lbl">Datatables</span>
-	            </span>
-	            <ul>
-	                <a href="datatables-net.php"><span class="lbl">Datatables.net</span></a></li>
-	                <a href="bootstrap-datatables.php"><span class="lbl">Bootstrap Table</span></a></li>
-	
-	                <!--<li><a href="datatables.php"><span class="lbl">Default</span></a></li>
-	                <li><a href="datatables-fixed-columns.php"><span class="lbl">Fixed Columns</span></a></li>
-	                <li><a href="datatables-reorder-rows.php"><span class="lbl">Reorder Rows</span></a></li>
-	                <li><a href="datatables-reorder-columns.php"><span class="lbl">Reorder Columns</span></a></li>
-	                <li><a href="datatables-resize-columns.php"><span class="lbl">Resize Columns</span></a></li>
-	                <li><a href="datatables-mobile.php"><span class="lbl">Mobile</span></a></li>
-	                <li><a href="datatables-filter-control.php"><span class="lbl">Filters</span></a></li>-->
-	            </ul>
-	        </li>
-	        <li class="green with-sub">
-	            <span>
-	                <i class="font-icon font-icon-widget"></i>
-	                <span class="lbl">Components</span>
-	            </span>
-	            <ul>
-	                <li><a href="widgets.php"><span class="lbl">Widgets</span></a></li>
-	                <li><a href="elements.php"><span class="lbl">Bootstrap UI</span></a></li>
-	                <li><a href="ui-datepicker.php"><span class="lbl">Date and Time Pickers</span></a></li>
-	                <li><a href="multipicker.php"><span class="lbl">Multi Picker</span></a></li>
-	                <li><a href="form-steps.php"><span class="lbl">Form Steps</span></a></li>
-	                <li><a href="components-upload.php"><span class="lbl">Upload</span></a></li>
-	                <li><a href="sweet-alerts.php"><span class="lbl">SweetAlert</span></a></li>
-	                <li><a href="tabs.php"><span class="lbl">Tabs</span></a></li>
-	                <li><a href="panels.php"><span class="lbl">Panels</span></a></li>
-	                <li><a href="notifications.php"><span class="lbl">Notifications</span></a></li>
-	                <li><a href="range-slider.php"><span class="lbl">Sliders</span></a></li>
-	                <li><a href="editor-summernote.php"><span class="lbl">Editors</span></a></li>
-	                <li><a href="nestable.php"><span class="lbl">Nestable</span></a></li>
-	                <li><a href="blockui.php"><span class="lbl">BlockUI</span></a></li>
-	                <li><a href="alerts.php"><span class="lbl">Alerts</span></a></li>
-	                <li><a href="player.php"><span class="lbl">Players</span></a></li>
-	            </ul>
-	        </li>
-	     <!--   <li class="gold">
-	            <a href="#">
-	                <i class="font-icon font-icon-speed"></i>
-	                <span class="lbl">Performance</span>
-	            </a>
-	        </li>-->
-	        <li class="pink-red">
-	            <a href="activity.php">
-	                <i class="font-icon font-icon-zigzag"></i>
-	                <span class="lbl">Activity</span>
-	            </a>
-	        </li>
-	        <li class="blue with-sub">
-	            <span>
-	                <i class="font-icon font-icon-user"></i>
-	                <span class="lbl">Profile</span>
-	            </span>
-	            <ul>
-	                <li><a href="profile.php"><span class="lbl">Version 1</span></a></li>
-	                <li><a href="profile-2.php"><span class="lbl">Version 2</span></a></li>
-	            </ul>
-	        </li>
-	        <li class="orange-red with-sub">
-	            <span>
-	                <i class="font-icon font-icon-help"></i>
-	                <span class="lbl">Support</span>
-	            </span>
-	            <ul>
-	                <li><a href="documentation.php"><span class="lbl">Docs (example)</span></a></li>
-	                <li><a href="faq.php"><span class="lbl">FAQ Simple</span></a></li>
-	                <li><a href="faq-search.php"><span class="lbl">FAQ Search</span></a></li>
-	            </ul>
-	        </li>
-	        <li class="red">
-	            <a href="contacts.php" class="label-right">
-	                <i class="font-icon font-icon-contacts"></i>
-	                <span class="lbl">Contacts</span>
-	                <span class="label label-custom label-pill label-danger">35</span>
-	            </a>
-	        </li>
-	        <li class="magenta opened">
-	            <a href="scheduler.php">
-	                <i class="font-icon font-icon-calend"></i>
-	                <span class="lbl">Calendar</span>
-	            </a>
-	        </li>
-	        <li class="grey with-sub">
-	            <span>
-	                <span class="glyphicon glyphicon-duplicate"></span>
-	                <span class="lbl">Pages</span>
-	            </span>
-	            <ul>
-	                <li><a href="email_templates.php"><span class="lbl">Email Templates</span></a></li>
-	                <li><a href="blank.php"><span class="lbl">Blank</span></a></li>
-	                <li><a href="empty.php"><span class="lbl">Empty List</span></a></li>
-	                <li><a href="prices.php"><span class="lbl">Prices</span></a></li>
-	                <li><a href="typography.php"><span class="lbl">Typography</span></a></li>
-	                <li><a href="sign-in.php"><span class="lbl">Login</span></a></li>
-	                <li><a href="sign-up.php"><span class="lbl">Register</span></a></li>
-	                <li><a href="reset-password.php"><span class="lbl">Reset Password</span></a></li>
-	                <li><a href="new-password.php"><span class="lbl">New Password</span></a></li>
-	                <li><a href="error-404.php"><span class="lbl">Error 404</span></a></li>
-	                <li><a href="error-500.php"><span class="lbl">Error 500</span></a></li>
-	                <li><a href="cards.php"><span class="lbl">Cards</span></a></li>
-	                <li><a href="avatars.php"><span class="lbl">Avatars</span></a></li>
-	                <li><a href="ribbons.php"><span class="lbl">Ribbons</span></a></li>
-	                <li><a href="icons-startui.php"><span class="lbl">Icons</span></a></li>
-	                <li><a href="invoice.php"><span class="lbl">Invoice</span></a></li>
-	                <li><a href="helpers.php"><span class="lbl">Helpers</span></a></li>
-	            </ul>
-	        </li>
-	        <li class="blue-dirty">
-	            <a href="list-tasks.php">
-	                <i class="font-icon font-icon-notebook"></i>
-	                <span class="lbl">Tasks</span>
-	            </a>
-	        </li>
-	        <li class="aquamarine">
-	            <a href="contacts-page.php">
-	                <i class="font-icon font-icon-mail"></i>
-	                <span class="lbl">Contact form</span>
-	            </a>
-	        </li>
-	        <li class="blue">
-	            <a href="files.php">
-	                <i class="font-icon glyphicon glyphicon-paperclip"></i>
-	                <span class="lbl">File Manager</span>
-	            </a>
-	        </li>
-	        <li class="gold">
-	            <a href="gallery.php">
-	                <i class="font-icon font-icon-picture-2"></i>
-	                <span class="lbl">Gallery</span>
-	            </a>
-	        </li>
-	        <li class="red">
-	            <a href="project.php">
-	                <i class="font-icon font-icon-case-2"></i>
-	                <span class="lbl">Project</span>
-	            </a>
-	        </li>
-	        <li class="brown with-sub">
-	            <span>
-	                <span class="font-icon font-icon-chart"></span>
-	                <span class="lbl">Charts</span>
-	            </span>
-	            <ul>
-	                <li><a href="charts-c3js.php"><span class="lbl">C3.js</span></a></li>
-	                <li><a href="charts-peity.php"><span class="lbl">Peity</span></a></li>
-	                <li><a href="charts-plottable.php"><span class="lbl">Plottable.js</span></a></li>
-	            </ul>
-	        </li>
-	        <li class="grey with-sub">
-	            <span>
-	                <span class="font-icon font-icon-burger"></span>
-	                <span class="lbl">Nested Menu</span>
-	            </span>
-	            <ul>
-	                <li><a href="#"><span class="lbl">Level 1</span></a></li>
-	                <li><a href="#"><span class="lbl">Level 1</span></a></li>
-	                <li class="with-sub">
-	                    <span>
-	                        <span class="lbl">Level 2</span>
-	                    </span>
-	                    <ul>
-	                        <li><a href="#"><span class="lbl">Level 2</span></a></li>
-	                        <li><a href="#"><span class="lbl">Level 2</span></a></li>
-	                        <li class="with-sub">
-	                            <span>
-	                                <span class="lbl">Level 3</span>
-	                            </span>
-	                            <ul>
-	                                <li><a href="#"><span class="lbl">Level 3</span></a></li>
-	                                <li><a href="#"><span class="lbl">Level 3</span></a></li>
-	                            </ul>
-	                        </li>
-	                    </ul>
-	                </li>
-	            </ul>
-	        </li>
-	    </ul>
-	
-	    <section>
-	        <header class="side-menu-title">Tags</header>
-	        <ul class="side-menu-list">
-	            <li>
-	                <a href="#">
-	                    <i class="tag-color green"></i>
-	                    <span class="lbl">Website</span>
-	                </a>
-	            </li>
-	            <li>
-	                <a href="#">
-	                    <i class="tag-color grey-blue"></i>
-	                    <span class="lbl">Bugs/Errors</span>
-	                </a>
-	            </li>
-	            <li>
-	                <a href="#">
-	                    <i class="tag-color red"></i>
-	                    <span class="lbl">General Problem</span>
-	                </a>
-	            </li>
-	            <li>
-	                <a href="#">
-	                    <i class="tag-color pink"></i>
-	                    <span class="lbl">Questions</span>
-	                </a>
-	            </li>
-	            <li>
-	                <a href="#">
-	                    <i class="tag-color orange"></i>
-	                    <span class="lbl">Ideas</span>
-	                </a>
-	            </li>
-	        </ul>
-	    </section>
-	</nav><!--.side-menu-->
+       </li>
+        <li class="grey with-sub">
+            <span>
+                <i class="font-icon font-icon-dashboard"></i>
+                <span class="lbl">Teachers</span>
+            </span>
+            <ul>
+                <li><a href="add-teacher.php">Add a Teacher</a></li>
+                <li><a href="manage-teacher.php">Manage</a></li>
+                
+            </ul>
+        </li>
+        <li class="grey with-sub">
+            <span>
+                <i class="font-icon font-icon-dashboard"></i>
+                <span class="lbl">HOD</span>
+            </span>
+            <ul>
+                <li><a href="add-hod.php">Add a HOD</a></li>
+                <li><a href="manage-hod.php">Manage</a></li>
+                
+            </ul>
+        </li>
+        <li class="grey with-sub">
+            <span>
+                <i class="font-icon font-icon-dashboard"></i>
+                <span class="lbl">Binding</span>
+            </span>
+            <ul>
+                <li><a href="index.php">Binding HOD to Department</a></li>
+                <li><a href="dashboard-top-menu.php">Binding Teachers to Periods</a></li>
+                
+            </ul>
+        </li>
+        <li class="grey with-sub">
+            <span>
+                <i class="font-icon font-icon-dashboard"></i>
+                <span class="lbl">Reset</span>
+            </span>
+            <ul>
+                <li><a href="index.php">Year Change</a></li>
+                <li><a href="semester-change.php">Semester change</a></li>
+                <li><a href="dashboard-top-menu.php">Backup</a></li>
+                
+            </ul>
+        </li>
+       
+        </ul>
+ 
+</nav><!--.side-menu-->
