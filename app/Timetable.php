c951c5b3b6264b942d5442c21508ebabafdcdf94
Shobi <ppshobi@gmail.com>
2017-01-31 21:02:45 +0530

    Function for deleting any existing entries in timetable for a teacher

--- app/Timetable.php
+++ app/Timetable.php
@@ -3,9 +3,16 @@
 	* 
 	*/
 	require_once('DB.php');
-	require_once('Department.php');
 	class Timetable
 	{
-		
+		public function delete_current_timetable($teacher_id){
+			$db=new Db();
+			$sql="DELETE FROM timetable WHERE teacher_id = '$teacher_id'";
+			$result=$db->query($sql);
+			if($result){				
+				return true;
+			}
+			return false;		
+		}
 	}
 ?>
