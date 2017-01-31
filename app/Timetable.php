0753ad1744d287556b8f3b2a23d44f989f5a9302
Shobi <ppshobi@gmail.com>
2017-01-31 21:10:27 +0530

    Set a peried for a teacher - function

--- app/Timetable.php
+++ app/Timetable.php
@@ -14,5 +14,19 @@
 			}
 			return false;		
 		}
+
+		public function set_a_peried($teacher_id, $day, $peried, $dept, $batch){
+			$db=new Db();
+			if ($dept == '' OR $batch == '') {
+				$dept=NULL;
+				$batch=NULL;
+			}
+			$sql="INSERT INTO timetable(teacher_id, day, peried, dept, batch) VALUES ('$teacher_id', '$day', '$p', '$dept', '$batch')";
+			$result=$db->query($sql);
+			if($result){				
+				return true;
+			}
+			return false;
+		}
 	}
 ?>
