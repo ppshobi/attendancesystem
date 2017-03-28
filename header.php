bb41a4eb99fe014039acd578f1d3b5387894b4e4
Shobi <ppshobi@gmail.com>
2017-03-28 20:17:34 +0530

    Removed logout button drop down

--- header.php
+++ header.php
@@ -21,9 +21,7 @@
 	                            <img src="img/avatar-2-64.png" alt="">
 	                        </button>
 	                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dd-user-menu">
-	                            <a class="dropdown-item" href="#"><span class="font-icon glyphicon glyphicon-user"></span>Profile</a>
-	                            <a class="dropdown-item" href="#"><span class="font-icon glyphicon glyphicon-cog"></span>Settings</a>
-	                            <a class="dropdown-item" href="#"><span class="font-icon glyphicon glyphicon-question-sign"></span>Help</a>
+	                           
 	                            <div class="dropdown-divider"></div>
 	                            <a class="dropdown-item" href="login.php?logout"><span class="font-icon glyphicon glyphicon-log-out"></span>Logout</a>
 	                        </div>
